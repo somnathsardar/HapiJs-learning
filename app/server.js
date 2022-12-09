31e949b8aae1cda55ad3267e154771f2e246c3f5
somnathsardar <githubsomnath1996@gmail.com>
2022-12-09 14:46:08 +0530

    Added request lifecycle to server.js file

--- app/server.js
+++ app/server.js
@@ -6,8 +6,37 @@ const routes = require('./routes')
 const server = Hapi.server({
     port: 8000,
     host: 'localhost'
-});
+})
 
-server.route(routes);
+server.route(routes)
+
+server.ext('onRequest', function (request, h) {
+    // console.log('onRequest');
+    return h.continue
+})
+server.ext('onPreAuth', function (request, h) {
+    // console.log('onPreAuth');
+    return h.continue
+})
+server.ext('onCredentials', function (request, h) {
+    // console.log('onCredentials');
+    return h.continue
+})
+server.ext('onPostAuth', function (request, h) {
+    // console.log('onPostAuth');
+    return h.continue
+})
+server.ext('onPreHandler', function (request, h) {
+    // console.log('onPreHandler');
+    return h.continue
+})
+server.ext('onPostHandler', function (request, h) {
+    // console.log('onPostHandler');
+    return h.continue
+})
+server.ext('onPreResponse', function (request, h) {
+    // console.log('onPreResponse');
+    return h.continue
+})
 
 module.exports = server
